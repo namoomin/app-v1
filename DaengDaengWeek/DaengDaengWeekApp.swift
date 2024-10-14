779daecd70038d43fbfc90046578e2421d766e94
Jini <yjkwon424@gmail.com>
2024-10-14 17:21:31 +0900

    Initial Commit

---
+++ DaengDaengWeek/DaengDaengWeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaengDaengWeekApp.swift
+//  DaengDaengWeek
+//
+//  Created by Jini on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DaengDaengWeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
